bec22fa163d81d31c1e3a3f39faa985c93297805
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 15:25:16 +0000

    automatically backed up by learn

--- classpractice.rb
+++ classpractice.rb
@@ -1,11 +1,20 @@
 def FacebookProfiles
     def initialize(name, age)
-      @name= name
+      @name = name
       @age = age
    end
    
-  def 
-  
+  def bio
+@bio = bio  
  end
  
+ def name
+   @name
+ end
+ 
+ attr_accessor :job
+ 
+ end
+ 
+ becca= FacebookProfiles.new
  
